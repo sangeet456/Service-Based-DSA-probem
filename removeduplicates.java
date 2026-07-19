445397b7bdce12afe525e46d9d716d2d6dde43d5
sangeet456 <sangeet40404@gmail.com>
2026-07-19 07:38:48 -0400

    hell

---
+++ removeduplicates.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class removeduplicates{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=sc.nextInt();
+            
+        }
+
+    }
+}
