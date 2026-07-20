549d3c3f57159e27afd8973b535183d9d455beeb
sangeet456 <sangeet40404@gmail.com>
2026-07-20 03:41:37 -0400

    hell

---
+++ mergeedsorted.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class mergeedsorted {
+    class Solution {
+
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int arr[] = new int[m+n];
+        for(int i=0;i<nums1.length;i++){
+            arr[i] = nums1[i];
+        }
+        for(int j=0;j<n;j++){
+            arr[m+j] = nums2[j];
+        }
+        Arrays.sort(arr);
+       for(int i=0;i<arr.length;i++){
+        nums1[i] = arr[i]; 
+       }
+    }
+}
+    
+}
