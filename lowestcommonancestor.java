540c7221bc065f81d189b19431ee5fdb1290c5bd
sangeet456 <sangeet40404@gmail.com>
2026-08-07 09:17:51 -0400

    new code

---
+++ lowestcommonancestor.java
@@ -0,0 +1,19 @@
+import javax.swing.tree.TreeNode;
+
+public class lowestcommonancestor {
+    
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //base condition//
+        if(root == null) return null;
+        if(root.val == p.val || root.val == q.val) return root;
+        TreeNode leftlca = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightlca = lowestCommonAncestor(root.right,p,q);
+        if(leftlca!=null && rightlca!=null) return root;
+        else if (leftlca !=null) return leftlca;
+        
+
+    return rightlca;
+    }
+}
+}
